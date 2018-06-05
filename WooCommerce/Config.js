eff8698fca15ec74b5d97aacfd149196580b6097
actooalizame <gndalmedo@gmail.com>
2018-06-05 16:17:40 -0400

    first commit

---
+++ WooCommerce/Config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Constants = {
+    URL: {
+        root: 'http://www.royalcar.cl'
+    },
+    Keys: {
+        ConsumerKey: 'ck_bcc23fa99e77c3f3c435374c2a1523a952cccda4',
+	    ConsumerSecret: 'cs_cd08945f5de67a754fde17392db5a60c841aebf1'
+    }
+}
+
+module.exports = Constants;
